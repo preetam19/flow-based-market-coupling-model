3e14d8c45834c7abac455a19a29b0c1ba6f60ab5
Preetam Gattogi <52251442+preetam19@users.noreply.github.com>
2021-05-28 15:32:10 +0200

    Update model_D-2_base_case.jl

--- model_D-2_base_case.jl
+++ model_D-2_base_case.jl
@@ -86,14 +86,14 @@ for horizon in 1:ceil(Int, length(T)/hours_per_horizon)
 
 	@constraint(m, line_cap_neg[t=Tsub, l=L], LINE_F[t,l] >= -get_line_cap(l))
 	println("Built constraints line_cap_neg.")
-
-	@constraint(m, line_cap_pos_cnec[t=Tsub, l=CNEC],
-	LINE_F[t,l] <= get_line_cap(l)*(1-frm))
-	println("Built constraints line_cap_pos for CNECs.")
-
-	@constraint(m, line_cap_neg_cnec[t=Tsub, l=CNEC],
-	LINE_F[t,l] >= -get_line_cap(l)*(1-frm))
-	println("Built constraints line_cap_neg for CNECs.")
+#
+# 	@constraint(m, line_cap_pos_cnec[t=Tsub, l=CNEC],
+# 	LINE_F[t,l] <= get_line_cap(l)*(1-frm))
+# 	println("Built constraints line_cap_pos for CNECs.")
+#
+# 	@constraint(m, line_cap_neg_cnec[t=Tsub, l=CNEC],
+# 	LINE_F[t,l] >= -get_line_cap(l)*(1-frm))
+# 	println("Built constraints line_cap_neg for CNECs.")
 
 	for t in Tsub
 		JuMP.fix(DELTA[t,68], 0)
