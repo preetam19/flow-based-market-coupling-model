e60c1997396e412fb697e996f1ed2896deceb12f
Preetam Gattogi <52251442+preetam19@users.noreply.github.com>
2021-05-28 15:30:04 +0200

    Update eval_results.jl

--- eval_results.jl
+++ eval_results.jl
@@ -1,14 +1,26 @@
 cd("./results/D-1_market_coupling")
-d_1_gen_costs = convert(Array{Float64,2}, CSV.read(string("df_d_1_gen_costs.csv")))
-d_1_nodal_price= convert(Array{Float64,2}, CSV.read(string("df_d_1_nodal_price.csv")))
+# d_1_gen_costs = convert(Array{Float64,2}, CSV.read(string("df_d_1_gen_costs.csv")))
+# d_1_nodal_price= convert(Array{Float64,2}, CSV.read(string("df_d_1_nodal_price.csv")))
+d_1_gen_costs =  CSV.read(string("df_d_1_gen_costs.csv"), DataFrame)
+d_1_gen_costs = Array{Float64,2}(d_1_gen_costs)
+d_1_nodal_price =  CSV.read(string("df_d_1_nodal_price.csv"), DataFrame)
+d_1_nodal_price = Array{Float64}(d_1_nodal_price)
 cd("..")
 cd("..")
 
 cd("./results/D-0_congestion_management")
-d_0_curt = convert(Array{Float64,2}, CSV.read(string("df_d_0_curt.csv")))
-d_0_rd_pos = convert(Array{Float64,2}, CSV.read(string("df_d_0_rd_pos.csv")))
-d_0_rd_neg = convert(Array{Float64,2}, CSV.read(string("df_d_0_rd_neg.csv")))
-d_0_redispatch_costs = convert(Array{Float64,2}, CSV.read(string("df_d_0_redispatch_costs.csv")))
+# d_0_curt = convert(Array{Float64,2}, CSV.read(string("df_d_0_curt.csv")))
+# d_0_rd_pos = convert(Array{Float64,2}, CSV.read(string("df_d_0_rd_pos.csv")))
+# d_0_rd_neg = convert(Array{Float64,2}, CSV.read(string("df_d_0_rd_neg.csv")))
+# d_0_redispatch_costs = convert(Array{Float64,2}, CSV.read(string("df_d_0_redispatch_costs.csv")))
+d_0_curt =  CSV.read(string("df_d_0_curt.csv"), DataFrame)
+d_0_curt = Array{Float64,2}(d_0_curt)
+d_0_rd_pos =  CSV.read(string("df_d_0_rd_pos.csv"), DataFrame)
+d_0_rd_pos = Array{Float64}(d_0_rd_pos)
+d_0_rd_neg =  CSV.read(string("df_d_0_rd_neg.csv"), DataFrame)
+d_0_rd_neg = Array{Float64}(d_0_rd_neg)
+d_0_redispatch_costs =  CSV.read(string("df_d_0_redispatch_costs.csv"), DataFrame)
+d_0_redispatch_costs = Array{Float64}(d_0_redispatch_costs)
 cd("..")
 cd("..")
 
@@ -21,16 +33,16 @@ function price_zones(a::Array{Float64,2})
     v_temp = vec(convert(Array, mapcols!(count_unique, DataFrame(transpose(a[:,1:3])))))
     df = DataFrame(OnePriceZone = sum(v_temp .== 1),
                    TwoPriceZones = sum(v_temp .== 2),
-                   ThreePriceZones = sum(v_temp .== 3))
+                   ThreePriceZones = sum(v_temp .== 3),:auto)
     return df
 end
 
 ## D-1 MARKET COUPLING
-# Generation costs:
+# 3 costs:
 d_1_costs_temp = round.(reshape(sum(d_1_gen_costs[:,1:3],dims=1),:)/1e6, digits=3)
 
 # Price zones:
-price_zones_temp = vec(convert(Array,price_zones(d_1_nodal_price)))
+# price_zones_temp = vec(convert(Array,price_zones(d_1_nodal_price)))
 
 ## D-0 CONGESTION MANAGEMENT
 # Amounts:
@@ -50,7 +62,7 @@ println(" \t \t Zone 1 \t Zone 2 \t Zone 3")
 println("D-1 Gen. costs \t ", d_1_costs_temp[1], "\t ", d_1_costs_temp[2], "\t ", d_1_costs_temp[3])
 println("-------------------------------------------------------------------")
 println(" \t \t 1 zone \t 2 zones \t 3 zones")
-println("Price zones [h]  ", price_zones_temp[1], "\t \t ", price_zones_temp[2], "\t \t ", price_zones_temp[3])
+# println("Price zones [h]  ", price_zones_temp[1], "\t \t ", price_zones_temp[2], "\t \t ", price_zones_temp[3])
 println("-------------------------------------------------------------------")
 println(" \t \t Pos. RD \t Neg. RD \t Curt.")
 println("Amounts [GWh] \t ", d_0_rd_pos_temp, "\t \t ", d_0_rd_neg_temp, "\t \t ", d_0_curt_temp)
